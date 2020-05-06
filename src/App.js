0234b17721e3ae702377df68b912e1be55f9dfec
Uladzislau Danilchyk <uladzislau_danilchyk@epam.com>
2020-05-06 11:55:21 +0300

    Cleaned-up initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Wrapper from "./hoc/Wrapper/Wrapper";
+import CQ5Component from "./components/CQ5Component/CQ5Component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <CQ5Component id="CQ5LoginBannerComponent" />
+    </Wrapper>
   );
 }
 
