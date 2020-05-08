8c2ef3ca97790bf57c112f12980878dff98906fb
Uladzislau Danilchyk <uladzislau_danilchyk@epam.com>
2020-05-08 18:15:37 +0300

    Last test works 1/2

--- src/components/CQ5Component/CQ5Component.test.js
+++ src/components/CQ5Component/CQ5Component.test.js
@@ -1,9 +1,10 @@
-jest.mock("isomorphic-fetch");
+jest.mock("../../api/CMSService");
 
 import React from "react";
-import { render, configure, shallow, mount } from "enzyme";
+import { render, configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import CQ5Component from "./CQ5Component";
+import getCMSComponent from "../../api/CMSService";
 
 configure({ adapter: new Adapter() });
 
@@ -13,7 +14,7 @@ describe("CQ5 Component works properly", () => {
   };
 
   beforeEach(() => {
-    jest.spyOn(React, "useEffect").mockImplementation((f) => f());
+    jest.spyOn(React, "useEffect").mockImplementation(React.useLayoutEffect);
   });
 
   afterEach(() => {
@@ -22,6 +23,16 @@ describe("CQ5 Component works properly", () => {
 
   it("should render nothing when it's fetching data", () => {
     const wrapper = render(<CQ5Component {...props} />);
-    expect(wrapper.html()).toBe('');
+    expect(wrapper.html()).toBe("");
+  });
+
+  it("should render html received by a call to BFF", () => {
+    getCMSComponent.mockReturnValue(
+      Promise.resolve(new Response("<h1>BFF response html</h1>"))
+    );
+
+    const wrapper = mount(<CQ5Component {...props} />);
+    expect(getCMSComponent).toHaveBeenCalledTimes(1);
+    expect(wrapper.props().content).toBe("<h1>BFF response html</h1>");
   });
 });
