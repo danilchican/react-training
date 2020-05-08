34bb32df98e0895df6d99962a0a90ce56db011e8
Uladzislau Danilchyk <uladzislau_danilchyk@epam.com>
2020-05-08 15:31:54 +0300

    Small fix

--- src/components/CQ5Component/CQ5Component.test.js
+++ src/components/CQ5Component/CQ5Component.test.js
@@ -1,39 +1,27 @@
+jest.mock("isomorphic-fetch");
+
 import React from "react";
 import { render, configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import CQ5Component from "./CQ5Component";
-import { waitForElement } from "@testing-library/react";
-import fetchMock from "jest-fetch-mock"
 
 configure({ adapter: new Adapter() });
 
 describe("CQ5 Component works properly", () => {
+  let props = {
+    cmsContext: { id: "CQ5Global_header", journey: "ADDLINE" },
+  };
+
   beforeEach(() => {
-    fetchMock.resetMocks();
+    jest.spyOn(React, "useEffect").mockImplementation((f) => f());
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("should render nothing when it's fetching data", () => {
-    const props = {
-      cmsContext: { id: "CQ5Global_header", journey: "ADDLINE" },
-    };
-
     const wrapper = render(<CQ5Component {...props} />);
-
-    expect(wrapper.html()).toBe(null);
-  });
-
-  it("should render html after call to BFF", async () => {
-    fetchMock.mockResponseOnce("<h1>test response</h1>");
-
-    const props = {
-      cmsContext: { id: "CQ5Global_header", journey: "ADDLINE" },
-    };
-    const wrapper = shallow(<CQ5Component {...props} />);
-   
-    await expect(fetchMock.mock.calls.length).toEqual(1)
-
-    // expect(axios.get).toHaveBeenCalledWith(
-    // "/stubs/CQ5Global_header.html?journey=ADDLINE"
-    // );
+    expect(wrapper.html()).toBe('');
   });
 });
