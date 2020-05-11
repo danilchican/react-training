ecf5cff86ff3beea3dd8ac07e34cad6dec745268
Uladzislau Danilchyk <uladzislau_danilchyk@epam.com>
2020-05-11 13:35:22 +0300

    Reworked AEM stubs

--- src/api/CMSService.js
+++ src/api/CMSService.js
@@ -2,13 +2,21 @@ import fetch from "isomorphic-fetch";
 
 const DEFAULT_JOURNEY = "ACQUISITION";
 
+const componentsToURLsMapping = {
+  CQ5Global_header:
+    "content/ee-shop/consumer/personalization/l0/product/watch/_jcr_content/slot-one-par.html",
+  CQ5LoginBannerComponent:
+    "content/ee-shop/consumer/personalization/l0/product/watch/_jcr_content/slot-two-par.html",
+};
+
 const getCMSComponent = async ({ id, journey = DEFAULT_JOURNEY }) => {
   try {
-    const response = await fetch(
-      `http://localhost:3000/stubs/${id}.html?journey=${journey}`
-    );
+    const componentUrl = componentsToURLsMapping[id];
+    const url = `http://localhost:18080/${componentUrl}?journey=${journey}`;
 
+    const response = await fetch(url);
     const content = await response.text();
+
     return content;
   } catch (e) {
     console.error(e);
